3bef7518a3f8e587acaf08db8cdd0bc6c9632539
Eleonora Nocentini Sköldebrink <eleonora.nocentini@gmail.com>
2023-01-23 11:12:53 +0100

    lagt till början till js koden

---
+++ js/script.js
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCuOHm2QYDm0vAZ0851gcyNoRXaGLDq4uU",
+  authDomain: "slutprojekt-versionshant-178fa.firebaseapp.com",
+  databaseURL: "https://slutprojekt-versionshant-178fa-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "slutprojekt-versionshant-178fa",
+  storageBucket: "slutprojekt-versionshant-178fa.appspot.com",
+  messagingSenderId: "366280612570",
+  appId: "1:366280612570:web:f3bf7376ce3120a6b1e3e3",
+  measurementId: "G-H52F2QQWM0"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
